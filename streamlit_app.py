ac275090b4f890541b2ea4011ed1de4feb543bd9
tiiuser <158547177+tiiuser@users.noreply.github.com>
2024-02-02 10:47:25 +0400

    Update streamlit_app.py
    more adjustments

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,8 @@ query_text = st.text_input('Enter your question:', placeholder = 'Please provide
 # Form input and query
 result = []
 with st.form('myform', clear_on_submit=False):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not query_text)
+    submitted = st.form_submit_button('Submit', disabled=not query_text)
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
             response = generate_response(openai_api_key, query_text)
